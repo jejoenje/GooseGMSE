d7ab9b6df02eebf7c240d10337f4dd1cc14cc67b
jejoenje <jeroenminderman@gmail.com>
2018-08-17 12:04:50 +0100

    Single change in example. Noncritical.

--- app.r
+++ app.r
@@ -9,8 +9,8 @@ library(kableExtra)
 source('goose_predict_gui.R')
 
 # # ### for testing only:
-# input <- list(input_name=data.frame(datapath=as.vector('~/Documents/docs/000_ConFooBio/gmse/data/example data UPDATED test.csv')),
-#    sims_in=100, yrs_in=10, maxHB_in=2000, target_in=32000)
+# input <- list(input_name=data.frame(datapath=as.vector('~/Downloads/Islay_Stirling_ADDED.xls')),
+#    sims_in=5, yrs_in=5, maxHB_in=2000, target_in=32000)
 # input$input_name$datapath <- as.vector(input$input_name$datapath)
 # iterations <- input$sims_in
 # years <- input$yrs_in
